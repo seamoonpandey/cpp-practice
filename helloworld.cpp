e8ed02f3edd0165a0233c438c6995a74da369dd4
Seamoon Pandey <hackedasocial@gmail.com>
2023-02-28 20:02:31 +0545

    initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    sleep(5);
+    system("cls");
+    return 0;
+}
---
+++ shlok.c
@@ -0,0 +1,50 @@
+#include<stdio.h> //header files or pre-processor directives
+// #include<string.h>
+
+//nothing but some piece of already written codes
+
+//function declaration
+// int addition(int x,int y){
+//     int sum;
+//     sum=x+y;
+//     return sum;
+// }
+
+
+
+// void main()
+// {
+//     int sum,sum1,sum2;
+//     sum =  addition(1,3);//function call
+//     sum1=  addition(3,4);//function call
+//     printf("%d %d",sum,sum1);
+// }
+
+
+//datatypes
+
+// void main()
+// {
+//     int x;
+//     float y;
+//     long z; 
+//     double a ;
+//     char b;
+//     char c[5]; //string declaration
+//     // char d[]={'a','b'};
+//     // strcpy(d,"cdef");
+//     char d[5];
+//     scanf("%s",d);
+    
+//     printf("%c",d);
+
+// }
+
+//Array
+
+
+int main()
+{
+    
+    return 0;
+}
